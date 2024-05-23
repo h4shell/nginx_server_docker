bee17a0764ca95aa4c01a1a9ad3f5583ce587a22
Lorenzo Fornara <fw1987@gmail.com>
2024-05-23 16:45:01 +0000

    add php nginx

--- infra/docker/nginx/entrypoint.sh
+++ infra/docker/nginx/entrypoint.sh
@@ -9,14 +9,22 @@ mkdir -p /usr/nginx/conf.d
 
 for item in "${hosts[@]}"; do
     echo "server {
-        listen 80;
+        listen 0.0.0.0:8080;
         server_name $item;
 
+        root /app/$item;
+
         location / {
-            root /usr/share/nginx/html/$item;
-            index index.html;
+            index index.php index.html; # File di indice predefiniti da cercare nella directory
         }
-    }" > /etc/nginx/conf.d/$item.conf
+
+        location ~ \.php$ {
+            # fastcgi_pass [PHP_FPM_LINK_NAME]:9000;
+            fastcgi_pass phpfpm:9000;
+            fastcgi_index index.php;
+            include fastcgi.conf;
+        }
+}" > /opt/bitnami/nginx/conf/server_blocks/$item.conf
 done
 
 nginx -g 'daemon off;'
